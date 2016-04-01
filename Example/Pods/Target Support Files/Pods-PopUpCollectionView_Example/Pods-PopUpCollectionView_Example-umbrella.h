64926ea19d91fb99163951b24a3ac9b9218b8a2f
Lawrence Tran <lawrence@tran.com>
2016-03-31 20:53:48 -0500

    commit pods.

---
+++ Example/Pods/Target Support Files/Pods-PopUpCollectionView_Example/Pods-PopUpCollectionView_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_PopUpCollectionView_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PopUpCollectionView_ExampleVersionString[];
+
